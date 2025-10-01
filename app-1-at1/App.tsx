8b4f30b135579e4316fa85695ca336f471e03117
GABRIEL DAMÁSIO DE ORNELAS <gabriel.ornelas@a.ucb.br>
2025-10-01 10:00:07 -0300

    [1.0] PRONTOOOOOOO

--- app-1-at1/App.tsx
+++ app-1-at1/App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { useState } from 'react';
-import { StyleSheet, Button, Image, FlatList, Alert, ActivityIndicator, Modal, Pressable, Switch, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Button, Image, FlatList, Alert, ActivityIndicator, Modal, Pressable, Switch, Text, TextInput, TouchableOpacity, View, ScrollView, SectionList } from 'react-native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
@@ -37,6 +37,21 @@ export default function App() {
     },
   ]
 
+  const DATA_SECTION = [
+    {
+      title: 'Dados',
+      data: ['Imagens', 'Texto', 'Ícones']
+    },
+    {
+      title: 'Status',
+      data: ['Barra de status', 'Carregamento']
+    },
+    {
+      title: 'Navegação',
+      data: ['Clickspots', 'Modais', 'Janelas']
+    },
+  ]
+
   type ItemProps = {title: string};
 
   const Item = ({title}: ItemProps) => (
@@ -44,6 +59,50 @@ export default function App() {
       <Text style={styles.title}>{title}</Text>
     </View>
   );
+
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const modalPages = [
+    (
+      <View key="page1">
+        <Text style={styles.modalText}>Olá Mundo! Esta é uma demonstração de um aplicativo em React Native e várias de suas funções. Essas funções incluem:</Text>
+        <FlatList style={{ maxHeight: 250 }}
+          data={DATA}
+          renderItem={({item}) => <Item title={item.title} />}
+          keyExtractor={item => item.id}
+        />
+      </View>
+    ),
+    (
+      <View key="page2">
+        <Text style={styles.modalText}>Além disso, é possível utilizar funções como:</Text>
+          <SectionList style={{ maxHeight: 550 }}
+            sections={DATA_SECTION}
+            keyExtractor={(item, index) => item + index}
+            renderItem={({item}) => (
+              <View style={styles.item}>
+                <Text style={styles.title}>{item}</Text>
+              </View>
+            )}
+            renderSectionHeader={({section: {title}}) => (
+              <Text style={styles.header}>{title}</Text>
+            )}
+          />
+      </View>
+    ),
+    (
+      <View key="page3">
+        <ScrollView style={styles.scroll}>
+          <Text style={styles.modalText}>
+            Esta é apenas uma pequena demonstração do que essa tecnologia é capaz. Você pode buscar muito mais informações acessando reactnative.dev ou pesquisando online.
+            Obrigado por testar nosso aplicativo. Em breve teremos nosso app de verdade, o PharmaGo, de entrega de medicamentos. Fique à vontade se quiser testá-lo.
+            Esta é apenas uma pequena demonstração do que essa tecnologia é capaz. Você pode buscar muito mais informações acessando reactnative.dev ou pesquisando online.
+            Obrigado por testar nosso aplicativo. Em breve teremos nosso app de verdade, o PharmaGo, de entrega de medicamentos. Fique à vontade se quiser testá-lo.
+          </Text>
+        </ScrollView>
+      </View>
+    ),
+  ]
 
   return (
     <SafeAreaProvider>
@@ -64,38 +123,58 @@ export default function App() {
           <Button title={'Enviar'} onPress={createTwoButtonAlert} />
 
           <Modal
-          animationType="slide"
-          transparent={true}
-          visible={modalVisible}
-          onRequestClose={() => {
-            Alert.alert('Modal has been closed.');
-            setModalVisible(!modalVisible);
-          }}>
-          <View style={styles.centeredView}>
-            <View style={styles.modalView}>
-              <Text style={styles.modalText}>Olá Mundo! Esta é uma demonstração de um aplicativo em React Native e várias de suas funções. Essas funções incluem:</Text>
-              <FlatList
-                data={DATA}
-                renderItem={({item}) => <Item title={item.title} />}
-                keyExtractor={item => item.id}
-              />
-              <TouchableOpacity style={styles.button}>
-              <Text>Entendi.</Text>
-              </TouchableOpacity>
-              <Pressable
-                style={[styles.button, styles.buttonClose]}
-                onPress={() => setModalVisible(!modalVisible)}>
-                <Text style={styles.textStyle}>Fechar</Text>
-              </Pressable>
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert('Modal has been closed.');
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+              {modalPages[currentPage]}
+                <View style={{ width: '100%' }}>
+                  <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: '100%' }}>
+                    {currentPage > 0 && (
+                      <TouchableOpacity
+                        style={[styles.button, { backgroundColor: '#aaa', marginRight: 10 }]}
+                        onPress={() => setCurrentPage(currentPage - 1)}
+                      >
+                        <Text style={styles.textStyle}>Voltar</Text>
+                      </TouchableOpacity>
+                    )}
+
+                    {currentPage < modalPages.length - 1 ? (
+                      <TouchableOpacity
+                        style={[styles.button, styles.buttonOpen]}
+                        onPress={() => setCurrentPage(currentPage + 1)}
+                      >
+                        <Text style={styles.textStyle}>Próximo</Text>
+                      </TouchableOpacity>
+                    ) : (
+                      <Pressable
+                        style={[styles.button, styles.buttonClose]}
+                        onPress={() => {
+                          setCurrentPage(0);
+                          setModalVisible(false);
+                        }}
+                      >
+                        <Text style={styles.textStyle}>Fechar</Text>
+                      </Pressable>
+                    )}
+                  </View>
+                </View>
+              </View>
             </View>
-          </View>
-        </Modal>
+          </Modal>
         <Pressable
           style={[styles.button, styles.buttonOpen]}
           onPress={() => setModalVisible(true)}>
           <Text style={styles.textStyle}>Saber Mais</Text>
         </Pressable>
-          <StatusBar style="auto" />
+        <StatusBar style="auto" />
+        
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -107,6 +186,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#ddd',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  scroll: {
+    maxHeight: 190
   },
   switchRow: {
     flexDirection: 'row',
@@ -121,6 +203,8 @@ const styles = StyleSheet.create({
   modalView: {
     margin: 20,
     backgroundColor: 'white',
+    justifyContent: 'flex-start',
+    width: '90%',
     borderRadius: 20,
     padding: 35,
     alignItems: 'center',
@@ -159,13 +243,18 @@ const styles = StyleSheet.create({
   },
    item: {
     backgroundColor: '#ffcbc2ff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
+    padding: 12,
+    marginVertical: 4,
+    marginHorizontal: 12,
     alignItems: 'center'
   },
+  header: {
+    fontSize: 24,
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
   title: {
-    fontSize: 20,
+    fontSize: 16,
   },
   input: {
     backgroundColor: '#fff',
